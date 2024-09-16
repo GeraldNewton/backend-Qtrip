7f93969f7c702cab2d100c6b597c6a9605d783d9
GeraldNewton <arpitnewton1@gmail.com>
2024-09-16 18:58:24 +0530

    final commit

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ module.exports={
     JWT_EXPIRESIN:process.env.JWT_EXPIRESIN,
     REGEX_USERPASS:new RegExp(process.env.REGEX_USERPASS),
     PORT:process.env.PORT,
-    RESERVATION_LIMIT:process.env.RESERVATION_LIMIT
+    RESERVATION_LIMIT:process.env.RESERVATION_LIMIT,
+    MONGODB_URL_ORIGINAL:process.env.MONGODB_URL_ORIGINAL
 }
